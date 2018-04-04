7eb65ac6baff113b5325f26143db14e2b5dd6a88
Clement CHAMEYRAT <cchameyr@e2r8p18.42.fr>
2018-04-04 14:40:52 +0200

    work in progress

--- incl/ft_nm.h
+++ incl/ft_nm.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 14:50:08 by cchameyr          #+#    #+#             */
-/*   Updated: 2018/04/02 15:23:21 by cchameyr         ###   ########.fr       */
+/*   Updated: 2018/04/04 14:27:13 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,17 @@
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
 
-#include "../..//perso/debug.h"
+#include "../../Perso/debug.h"
+
+# define ARCH_32 1
+# define ARCH_64 2
+
+typedef struct	s_data
+{
+	char	*ptr;
+	char	arch;
+	void	*header;
+	void	*first_load_command;
+}				t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,26 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 14:50:14 by cchameyr          #+#    #+#             */
-/*   Updated: 2018/04/02 16:30:31 by cchameyr         ###   ########.fr       */
+/*   Updated: 2018/04/04 14:28:32 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/ft_nm.h"
 
-void	print_output(int nsyms, int symoff, int stroff, char *ptr)
+char	handle_symtab_sect_64(t_data *nm_data, char n_sect)
+{
+	
+	return (0);
+}
+
+void	print_output(t_data *nm_data, int nsyms, int symoff, int stroff)
 {
 	int					i;
 	char				*stringtable;
+	char				*ptr;
 	struct nlist_64		*array;
 
+	ptr = nm_data->ptr;
 	array = (void *)ptr + symoff;
 	stringtable = (void *)ptr + stroff;
 	i = -1;
@@ -29,16 +37,52 @@ void	print_output(int nsyms, int symoff, int stroff, char *ptr)
 		else if (type == N_ABS)
 			type = 'A';
 		else if (type == N_SECT)
-			; //TODO
+			type = handle_symtab_sect_64(nm_data, array[i].n_sect);
 		else if (type == N_PBUD)
 			;
 		else if (type == N_INDR)
 			type = 'I';
-		ft_printf("%016llx [%d] %s\n", array[i].n_value, array[i].n_type, stringtable + array[i].n_un.n_strx);
+		if (type == 'U')
+			ft_printf("                 %c %s\n", type, stringtable + array[i].n_un.n_strx);
+		else
+			ft_printf("%016llx %c %s\n", array[i].n_value, type, stringtable + array[i].n_un.n_strx);
 	}
 }
 
-void	handle_magic_64(char *ptr)
+char	*get_section_name_64(t_data *nm_data, char n_sect)
+{
+	int					i;
+	int					j;
+	int					n_checked;
+	int					ncmds;
+	struct load_command	*lc;
+
+	ncmds = ((struct mach_header_64 *)nm_data->header)->ncmds;
+	lc = (struct load_command *)nm_data->first_load_command;
+	i = -1;
+	n_checked = 0;
+	while (++i < ncmds)
+	{
+		if (lc->cmd == LC_SEGMENT_64)
+		{
+			struct segment_command_64 *segm;
+			segm = (struct segment_command_64 *)lc;
+			struct section_64 *sect = (void *)segm + sizeof(*segm);
+			j = -1;
+			while (++j < segm->nsects)
+			{
+				n_checked++;
+				if (n_checked == n_sect)
+					return (sect->sectname);
+				sect = (void *)sect + sect->size;
+			}
+		}
+		lc = (void *)lc + lc->cmdsize;
+	}
+	return (NULL);
+}
+
+void	handle_magic_64(t_data *nm_data, char *ptr)
 {
 	int						ncmds;
 	int						i;
@@ -47,31 +91,42 @@ void	handle_magic_64(char *ptr)
 	struct symtab_command	*sym;
 
 	header = (struct mach_header_64 *)ptr;
-	ncmds = header->ncmds;
+	nm_data->header = (void *)header;
 	lc = (void *)ptr + sizeof(struct mach_header_64);
+	nm_data->first_load_command = lc;
+	ncmds = header->ncmds;
 	i = -1;
 	while (++i < ncmds)
 	{
 		if (lc->cmd == LC_SYMTAB)
 		{
 			sym = (struct symtab_command*)lc;
-			print_output(sym->nsyms, sym->symoff, sym->stroff, ptr);
+			print_output(nm_data, sym->nsyms, sym->symoff, sym->stroff);
 			break;
 		}
 		lc = (void *) lc + lc->cmdsize;
 	}
 }
 
-void	ft_nm(char *ptr)
+
+
+void	ft_nm(t_data *nm_data, char *ptr)
 {
 	int		magic_number;
 
 	magic_number = *(int *)ptr;
+	nm_data->ptr = ptr;
 	ft_printf("magic number : %x\n", magic_number);
-	if (magic_number == MH_MAGIC_64) 
-		handle_magic_64(ptr);
+	if (magic_number == MH_MAGIC_64)
+	{
+		nm_data->arch = ARCH_64;
+		handle_magic_64(nm_data, ptr);
+	}
 	else if (magic_number == MH_MAGIC)
-		; // TODO here
+	{
+		nm_data->arch = ARCH_32
+			; // TODO here
+	}
 }
 
 int		main(int argc, char **argv)
@@ -79,6 +134,7 @@ int		main(int argc, char **argv)
 	int				fd;
 	char			*ptr;
 	struct stat		buff;
+	t_data			nm_data;
 
 	if (argc != 2)
 	{
@@ -100,7 +156,7 @@ int		main(int argc, char **argv)
 		ft_printf("mmap error\n");
 		return (EXIT_FAILURE);
 	}
-	ft_nm(ptr);
+	ft_nm(&nm_data, ptr);
 	if (munmap(ptr, buff.st_size) < 0)
 	{
 		ft_printf("munmap error\n");
