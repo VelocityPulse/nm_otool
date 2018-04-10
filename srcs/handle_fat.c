17b8e49c2edbc17d2fbc2b5bf165d73641623eb1
Clement CHAMEYRAT <cchameyr@e2r8p18.42.fr>
2018-04-10 11:00:30 +0200

    debug fat mode

--- srcs/handle_fat.c
+++ srcs/handle_fat.c
@@ -6,18 +6,21 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 11:36:52 by cchameyr          #+#    #+#             */
-/*   Updated: 2018/04/09 16:08:10 by cchameyr         ###   ########.fr       */
+/*   Updated: 2018/04/10 10:59:45 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/ft_nm.h"
 
-void	init_sub_nm_data(t_data *sub_nm_data, char *ptr, int offset)
+void	init_sub_nm_data(t_data *nm_data, t_data *sub_nm_data, char *ptr,
+		int offset)
 {
+	trigger_false_pointer(nm_data, ptr);
 	sub_nm_data->nlist64_list = NULL;
 	sub_nm_data->nlist32_list = NULL;
 	sub_nm_data->ptr = ptr;
-	sub_nm_data->ptr_offset = offset;
+	if (offset !=  0)
+		sub_nm_data->ptr_offset = offset;
 }
 
 void	handle_fat64(t_data *nm_data, char *ptr)
@@ -35,6 +38,7 @@ void	handle_fat64(t_data *nm_data, char *ptr)
 	while (++i < nm_bsp64(nm_data, f_header->nfat_arch))
 	{
 		offset = nm_bsp64(nm_data, fa->offset);
+		sub_nm_data.ptr_offset = nm_bsp64(nm_data, fa->size);
 		trigger_false_pointer(nm_data, (void *)ptr + offset);
 		if (nm_bsp64(nm_data, fa->cputype) == CPU_TYPE_X86_64)
 			break ;
@@ -42,9 +46,8 @@ void	handle_fat64(t_data *nm_data, char *ptr)
 		trigger_false_pointer(nm_data, (void *)fa);
 	}
 	f_header = (void *)ptr + offset;
-	trigger_false_pointer(nm_data, (void *)f_header);
 	trigger_false_pointer(nm_data, nm_data->ptr + nm_bsp64(nm_data, fa->size));
-	init_sub_nm_data(&sub_nm_data, (void *)f_header,
+	init_sub_nm_data(nm_data, &sub_nm_data, (void *)f_header,
 			nm_bsp64(nm_data, fa->size));
 	ft_nm(&sub_nm_data, (void *)f_header);
 }
@@ -64,6 +67,7 @@ void	handle_fat32(t_data *nm_data, char *ptr)
 	while (++i < nm_bsp32(nm_data, f_header->nfat_arch))
 	{
 		offset = nm_bsp32(nm_data, fa->offset);
+		sub_nm_data.ptr_offset = nm_bsp32(nm_data, fa->size);
 		trigger_false_pointer(nm_data, (void *)ptr + offset);
 		if (nm_bsp32(nm_data, fa->cputype) == CPU_TYPE_X86_64)
 			break ;
@@ -71,9 +75,8 @@ void	handle_fat32(t_data *nm_data, char *ptr)
 		trigger_false_pointer(nm_data, (void *)fa);
 	}
 	f_header = (void *)ptr + offset;
-	trigger_false_pointer(nm_data, (void *)f_header);
 	trigger_false_pointer(nm_data, nm_data->ptr + nm_bsp32(nm_data, fa->size));
-	init_sub_nm_data(&sub_nm_data, (void *)f_header,
+	init_sub_nm_data(nm_data, &sub_nm_data, (void *)f_header,
 			nm_bsp32(nm_data, fa->size));
 	ft_nm(&sub_nm_data, (void *)f_header);
 }
